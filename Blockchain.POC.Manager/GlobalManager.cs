26c978d0a6397fe2567152e3e03add4226a1abe1
Anas Bouarfa <anas.bouarfa@accenture.com>
2019-05-15 11:37:53 +0000

    Test Updated

--- Blockchain.POC.Manager/GlobalManager.cs
+++ Blockchain.POC.Manager/GlobalManager.cs
@@ -1,12 +1,31 @@
-﻿namespace Blockchain.POC.Manager
+﻿using System;
+
+namespace Blockchain.POC.Manager
 {
-    public partial class GlobalManager : IGlobalManager
+    public partial class GlobalManager : IGlobalManager, IDisposable
     {
         private readonly int usedPort;
 
         public GlobalManager(int port)
         {
             usedPort = port;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // free managed resources
+            }
+            // free native resources if there are any.
         }
     }
 }
---
+++ testCommon/Serialization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blockchain.POC.Entities;
+using Blockchain.POC.Manager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testCommon
+{
+    [TestClass]
+    public class Serialization
+    {
+        [TestMethod]
+        public void Deserialization()
+        {
+            using (var _globalManager = new GlobalManager(12900))
+            {
+                var chain = _globalManager.LoadLocalBlockChain();
+
+                Transaction transaction = chain.Blocks.SelectMany(s => s.Transactions).FirstOrDefault();
+
+                Assert.AreEqual("anas", transaction.FromAddress);
+            }
+        }
+    }
+}
